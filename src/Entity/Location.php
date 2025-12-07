e46da7c77ff4795eece3fe8d44b8d61712792397
softjkeee <softjkeee@gmail.com>
2025-12-07 19:47:41 +0300

    Location option fields

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -8,18 +8,26 @@ namespace Kwarcek\FurgonetkaRestApi\Entity;
  */
 class Location extends Entity
 {
-    public Coordinate $coordinate;
+    public ?Coordinate $coordinate = null;
     public ?string $searchPhrase = null;
     public Address $address;
     public ?float $pointsMaxDistance = 10.00;
 
     public function toArray(): array
     {
-        return [
+        $location = [
           'coordinates' => $this->coordinate->toArray(),
           'search_phrase' => $this->searchPhrase,
           'address' => $this->address->toArray(),
           'points_max_distance' => $this->pointsMaxDistance,
         ];
+
+        foreach ($location as $k => $v) {
+            if ($v === null) {
+                unset($location[$k]);
+            }
+        }
+
+        return $location;
     }
 }
