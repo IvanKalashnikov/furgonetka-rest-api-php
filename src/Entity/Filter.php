d6d5ff2ae33e47cae6ec8f9f0bf21af8dfcc7dbc
softjkeee <softjkeee@gmail.com>
2025-12-07 13:07:19 +0300

    allow filter via point_id

--- src/Entity/Filter.php
+++ src/Entity/Filter.php
@@ -8,18 +8,28 @@ namespace Kwarcek\FurgonetkaRestApi\Entity;
  */
 class Filter extends Entity
 {
-    public array $services;
-    public array $pointTypes;
-    public MapBound $mapBound;
+    public ?array $services = null;
+    public ?array $pointTypes = null;
+    public ?MapBound $mapBound = null;
     public string $type = '';
+    public ?string $pointId = null;
 
     public function toArray(): array
     {
-        return [
+        $filters = [
             'services' => $this->services,
             'point_types' => $this->pointTypes,
             'map_bounds' => $this->mapBound->toArray(),
             'type' => $this->type,
+            'point_id' => $this->pointId,
         ];
+
+        foreach ($filters as $key => $value) {
+            if ($value === null) {
+                unset($filters[$key]);
+            }
+        }
+
+        return $filters;
     }
 }
